0a99c9f29f8be69fba8407ea86c17d8fd2dadd3b
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-10 18:50:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,21 @@ def second_challenge
   epic_tragedy = {
     :montague => {
      
-      }, 
+      }, epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
+ 
     :capulet => {
      
       }}
